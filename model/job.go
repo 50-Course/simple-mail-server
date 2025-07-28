534b16fb4dedd3ce449d251e6659d87d28dc1856
50-Course <erioluwaadeodu@my.uopeople.edu>
2025-07-28 13:36:47 +0100

    feat: implement model methods and validators

---
+++ model/job.go
@@ -0,0 +1,46 @@
+// houses the everything related to the job struct
+
+package model
+
+import (
+	"errors"
+	"regexp"
+)
+
+var ErrRequiredFields = errors.New("all fields are required")
+var ErrInvalidEmailFormat = errors.New("invalid email format")
+
+// Represents the incoming email request payload
+type JobRequest struct {
+	To      string `json:"to"`
+	Subject string `json:"subject"`
+	Body    string `json:"body"`
+}
+
+// Represents structured API response
+type JobResponse struct {
+	Status  string `json:"status"`
+	Message string `json:"message"`
+}
+
+// Represents a single job in the queue
+type EmailJob struct {
+	JobRequest
+	Retries int
+}
+
+// Validate checks if the email job request is valid
+// this ould help us match emails like: word@word.word
+var emailPattern = regexp.MustCompile(`^[\w\.-]+@[\w\.-]+\.\w+$`)
+
+// Ensures required fields are present and email is valid
+func validateJob(req JobRequest) error {
+	if req.To == "" || req.Subject == "" || req.Body == "" {
+		return ErrRequiredFields
+	}
+	if !emailPattern.MatchString(req.To) {
+		return ErrInvalidEmailFormat
+	}
+	return nil
+}
+
